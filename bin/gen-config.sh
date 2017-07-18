a779710d72654ec47e31224c8206b86251280ab0
hwaterke <hwaterke@users.noreply.github.com>
2017-07-18 19:04:39 +0200

    Using the alpine version of the ruby image to generate the configs

--- bin/gen-config.sh
+++ bin/gen-config.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -it -v "$(pwd)/..:/app" -w /app ruby ruby ./bin/gen-config.rb
+docker run --rm -it -v "$(pwd)/..:/app" -w /app ruby:alpine ruby ./bin/gen-config.rb
